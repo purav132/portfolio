be820ab4f2dcb873906e1e9cd7a43a04920ef9e9
Purav Kansara <98770330+purav132@users.noreply.github.com>
2023-02-22 17:36:36 +0530

    Add files via upload

---
+++ project/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/About.js';
+import Info from './components/Info.js';
+import Interests from './components/Interests.js';
+import Footer from './components/Footer.js';
+
+export default function App(){
+    return (
+        <div className='container'>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </div>
+    )
+}
---
+++ project/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <div className='footer'>
+            <button className="fa-brands fa-twitter"></button>
+           <button className="fa-brands fa-facebook-f"></button>
+           <button className="fa-brands fa-instagram"></button>
+           <button className="fa-brands fa-github"></button>
+        </div>
+    )
+}
